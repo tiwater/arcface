36952eb4e1e947f856c068a200e54d888f55f4cc
Sawyer <35255629+shaoyie@users.noreply.github.com>
2024-09-14 10:47:22 +0800

    feat: add the quntized version

--- inference_onnx.py
+++ inference_onnx.py
@@ -1,13 +1,12 @@
 # arcFace.py
-
-from rknnlite.api import RKNNLite
 import onnxruntime as ort
 from PIL import Image
 import numpy as np
 import os
 
-
-model = ort.InferenceSession('./model/arcface.onnx')
+providers = ['CPUExecutionProvider']
+model = ort.InferenceSession('./model/arcface_int8.onnx', providers=providers)
+# model = ort.InferenceSession('./model/arcface.onnx')
 
 img = Image.open('./dataset/testImg/0204.jpg')
 img = img.resize((112, 112))
@@ -16,7 +15,7 @@ img = np.array(img).astype(np.float32)/255.0
 img = (img * 2) - 1
 img = img.transpose((2, 0, 1))[np.newaxis, ...]
 
-output_src = model.run(None, {'data': img})[0][0]
+output_src = model.run(None, {'input': img})[0][0]
 
 # Directory containing target images
 target_dir = './dataset/testImg'
@@ -33,7 +32,7 @@ for filename in os.listdir(target_dir):
         img_target = img_target.transpose((2, 0, 1))[np.newaxis, ...]
 
         # Get inference for the target image
-        output_target = model.run(None, {'data': img_target})[0][0]
+        output_target = model.run(None, {'input': img_target})[0][0]
 
         # Calculate the cosine similarity
         cos_sim = np.dot(output_src, output_target) / (np.linalg.norm(output_src) * np.linalg.norm(output_target))
